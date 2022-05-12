b877135b96e8450452531245fcb2d206a0505228
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-05-12 13:49:47 -0500

    build to docs

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs';
+//import { getGifs } from '../helpers/getGifs';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
