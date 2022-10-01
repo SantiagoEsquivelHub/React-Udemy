c1470d6a0ad71e61d970328c41cc14bc2670c3b7
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-10-01 12:40:55 -0500

    Empezar proyecto de JournalApp

---
+++ 08-journal-app/src/journal/routes/JournalRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { JournalPage } from '../views/JournalPage'
+
+export const JournalRoutes = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<JournalPage />} />
+            <Route path='/*' element={<Navigate to='/' />} />
+        </Routes>
+    )
+}
